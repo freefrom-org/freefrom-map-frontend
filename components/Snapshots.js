2d9c936d11ac95e156b54d234b82f32d228c757d
Metamas <metamas@tutanota.com>
2022-11-21 10:36:25 -0800

    Make snapshot linebreaks responsive

--- components/Snapshots.js
+++ components/Snapshots.js
@@ -6,53 +6,55 @@ function Snapshots() {
         <div className={`${styles['snapshot-grid']}`}>
             <div className={`${styles['snapshot-card']}`} style={{ backgroundColor: '#ffb801' }}>
                 <div className={`${styles['snapshot-label-count']}`}>
-                    0<br />
+                    0{" "}
+                    <br className={`${styles['linebreak']}`} />
                     States
                 </div>
                 <div className={`${styles['snapshot-label-category']}`}>Model State</div>
             </div>
             <div className={`${styles['snapshot-card']}`} style={{ backgroundColor: '#46cece' }}>
                 <div className={`${styles['snapshot-label-count']}`}>
-                    2<br />
+                    2{" "}
+                    <br className={`${styles['linebreak']}`} />
                     States
                 </div>
                 <div className={`${styles['snapshot-label-category']}`}>
-                    Financial
-                    <br />
-                    Security
-                    <br />
+                    Financial{" "}
+                    <br className={`${styles['linebreak']}`} />
+                    Security{" "}
+                    <br className={`${styles['linebreak']}`} />
                     Friendly
                 </div>
             </div>
             <div className={`${styles['snapshot-card']}`} style={{ backgroundColor: '#ace6e2' }}>
                 <div className={`${styles['snapshot-label-count']}`}>
-                    11
-                    <br />
+                    11{" "}
+                    <br className={`${styles['linebreak']}`} />
                     States
                 </div>
                 <div className={`${styles['snapshot-label-category']}`}>Taking Steps</div>
             </div>
             <div className={`${styles['snapshot-card']}`} style={{ backgroundColor: '#ff9999' }}>
                 <div className={`${styles['snapshot-label-count']}`}>
-                    17
-                    <br />
+                    17{" "}
+                    <br className={`${styles['linebreak']}`} />
                     States
                 </div>
                 <div className={`${styles['snapshot-label-category']}`}>
-                    Some
-                    <br />
+                    Some{" "}
+                    <br className={`${styles['linebreak']}`} />
                     Accountability
                 </div>
             </div>
             <div className={`${styles['snapshot-card']}`} style={{ backgroundColor: '#f06448' }}>
                 <div className={`${styles['snapshot-label-count']}`}>
-                    21
-                    <br />
+                    21{" "}
+                    <br className={`${styles['linebreak']}`} />
                     States
                 </div>
                 <div className={`${styles['snapshot-label-category']}`}>
-                    Little
-                    <br />
+                    Little{" "}
+                    <br className={`${styles['linebreak']}`} />
                     Accountability
                 </div>
             </div>
