b58669cb0fe6c0b7cb14e21756b61e0a55a91d10
Metamas <metamas@tutanota.com>
2023-02-21 11:34:40 -0800

    Fix share/form button group misalignment

--- components/common/ShareButtons.js
+++ components/common/ShareButtons.js
@@ -8,7 +8,7 @@ import { trackEvent } from 'utils'
 const ShareButtons = ({ className, context }) => (
     <div className={className}>
         <a
-            className='btn btn-sm btn-primary mr-2 mt-2'
+            className='btn btn-sm btn-primary mr-2 mb-2'
             href='https://www.facebook.com/sharer/sharer.php?u=https://mapandscorecard.freefrom.org'
             target='_blank'
             rel='noopener noreferrer'
@@ -17,7 +17,7 @@ const ShareButtons = ({ className, context }) => (
             <FontAwesomeIcon icon={faFacebook} className='mr-1' /> Share
         </a>
         <a
-            className='btn btn-sm btn-primary mr-2 mt-2'
+            className='btn btn-sm btn-primary mr-2 mb-2'
             // eslint-disable-next-line max-len
             href='https://twitter.com/intent/tweet?text=The%20no.%201%20obstacle%20to%20safety%20for%20survivors%20is%20financial%20insecurity.%20Check%20out%20FreeFrom’s%20National%20Survivor%20Financial%20Security%20Policy%20Map%20and%20Scorecard%20to%20see%20how%20your%20state%20measures%20up%20in%20supporting%20survivor%20financial%20security%20they%20need%20to%20stay%20safe:&url=https://mapandscorecard.freefrom.org'
             target='_blank'
@@ -27,7 +27,7 @@ const ShareButtons = ({ className, context }) => (
             <FontAwesomeIcon icon={faTwitter} className='mr-1' /> Tweet
         </a>
         <a
-            className='btn btn-sm btn-primary mt-2'
+            className='btn btn-sm btn-primary mb-2'
             // eslint-disable-next-line max-len
             href="mailto:''?subject=National Survivor Financial Security Policy Map And Scorecard&body=Take a look at FreeFrom's National Survivor Financial Security Policy Map And Scorecard: https://mapandscorecard.freefrom.org"
             target='_blank'
