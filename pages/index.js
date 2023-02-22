ee99a363c220bef47d601a9da7f345d415845d3d
Metamas <metamas@tutanota.com>
2023-02-22 07:09:35 -0800

    Make an editable StateWatchlist component

--- pages/index.js
+++ pages/index.js
@@ -3,227 +3,145 @@ import PropTypes from 'prop-types'
 
 import ReportMissingInfo from 'components/common/ReportMissingInfo'
 import ShareButtons from 'components/common/ShareButtons'
-import StateUpdates from 'components/common/StateUpdates'
-import TakeAction from 'components/common/TakeAction'
 import SharedLayout from 'components/SharedLayout'
-import StatesList from 'components/StatesList'
 import Snapshots from 'components/Snapshots'
+import StateUpdates from 'components/common/StateUpdates'
+import StatesList from 'components/StatesList'
+import StateWatchlist from 'components/StateWatchlist'
+import TakeAction from 'components/common/TakeAction'
 import UsMap from 'components/UsMap'
-import styles from "components/common/Common.module.css"
+
+import styles from 'components/common/Common.module.css'
 import { getStates } from 'lib/contentful-api'
 
-function Home({ states }) {
-    const visibleComponent = 'list'
-    const showList = visibleComponent === 'list'
-    const mapClass = showList ? ' d-none d-md-block' : ''
-    const listClass = showList ? '' : ' d-none d-md-block'
-    return (
-        <SharedLayout>
-            <>
-                <h1 className='mt-3'>The National Survivor Financial Security Policy Map and Scorecard</h1>
-                <p className='mb-4'>How well does your state support survivors’ financial security?</p>
-            </>
+function Home({ allStates, watchlistStates }) {
+  const visibleComponent = 'list'
+  const showList = visibleComponent === 'list'
+  const mapClass = showList ? ' d-none d-md-block' : ''
+  const listClass = showList ? '' : ' d-none d-md-block'
 
-            <div className='d-flex flex-row flex-fill'>
-                <div className={`col-md-3 p-0${listClass}`} style={{ minWidth: '300px' }}>
-                    <StatesList states={states} />
-                </div>
+  return (
+    <SharedLayout>
+      <h1 className='mt-3'>The National Survivor Financial Security Policy Map and Scorecard</h1>
+      <p className='mb-4'>How well does your state support survivors’ financial security?</p>
 
-                <div className={`col-md-9${mapClass}`}>
-                    <div aria-hidden='true' className={`mb-4 ml-3 d-md-flex flex-row justify-content-start${mapClass}`}>
-                        <div className='d-flex flex-column'>
-                            <h4 className='mb-1'>Key</h4>
-                            <div className={styles['map-key-container']}>
-                                <span className={styles['map-key-item']} style={{backgroundColor: '#FFB400'}}>Model State</span>
-                                <span className={styles['map-key-item']} style={{backgroundColor: '#00CFCE'}}>Financial Security Friendly</span>
-                                <span className={styles['map-key-item']} style={{backgroundColor: '#AAE5E1'}}>Taking Steps</span>
-                                <span className={styles['map-key-item']} style={{backgroundColor: '#FF9194'}}>Some Accountability</span>
-                                <span className={styles['map-key-item']} style={{backgroundColor: '#FF583D'}}>Little Accountability</span>
-                            </div>
-                        </div>
-                    </div>
-                    <UsMap states={states} />
-                </div>
+      <div className='d-flex flex-row flex-fill'>
+        <div className={`col-md-3 p-0${listClass}`} style={{ minWidth: '300px' }}>
+          <StatesList states={allStates} />
+        </div>
+
+        <div className={`col-md-9${mapClass}`}>
+          <div aria-hidden='true' className={`mb-4 ml-3 d-md-flex flex-row justify-content-start${mapClass}`}>
+            <div className='d-flex flex-column'>
+              <h4 className='mb-1'>Key</h4>
+              <div className={styles['map-key-container']}>
+                <span className={styles['map-key-item']} style={{ backgroundColor: '#FFB400' }}>
+                  Model State
+                </span>
+                <span className={styles['map-key-item']} style={{ backgroundColor: '#00CFCE' }}>
+                  Financial Security Friendly
+                </span>
+                <span className={styles['map-key-item']} style={{ backgroundColor: '#AAE5E1' }}>
+                  Taking Steps
+                </span>
+                <span className={styles['map-key-item']} style={{ backgroundColor: '#FF9194' }}>
+                  Some Accountability
+                </span>
+                <span className={styles['map-key-item']} style={{ backgroundColor: '#FF583D' }}>
+                  Little Accountability
+                </span>
+              </div>
             </div>
+          </div>
+          <UsMap states={allStates} />
+        </div>
+      </div>
 
-            <div className='d-md-flex flex-row justify-content-start mt-4'>
-                <div className='d-flex flex-column flex-md-row' style={{ minWidth: '60%' }}>
-                    <div className='pr-3 mb-3'>
-                        <ShareButtons className='d-flex flex-row flex-nowrap' context='homepage' />
-                    </div>
-                    <div className='pr-3 mb-3'>
-                        <StateUpdates />
-                    </div>
-                    <div className='pr-3'>
-                        <ReportMissingInfo />
-                    </div>
-                </div>
-            </div>
+      <div className='d-md-flex flex-row justify-content-start mt-4'>
+        <div className='d-flex flex-column flex-md-row' style={{ minWidth: '60%' }}>
+          <div className='pr-3 mb-3'>
+            <ShareButtons className='d-flex flex-row flex-nowrap' context='homepage' />
+          </div>
+          <div className='pr-3 mb-3'>
+            <StateUpdates />
+          </div>
+          <div className='pr-3'>
+            <ReportMissingInfo />
+          </div>
+        </div>
+      </div>
 
-            <>
+      <StateWatchlist states={watchlistStates} />
 
-                <h2 className='mt-5 mt-3'>States to Watch</h2>
-                <div className='d-flex flex-column col-lg-10 m-0 p-0'>
-                    <div 
-                        className='d-flex flex-row flex-wrap flex-md-nowrap mb-4'
-                        style={{ backgroundColor: '#fff', border: '1px solid #DDDDDD', padding: '1.5rem'}}
-                    >
-                        <div className='mb-2 mb-md-0 mr-md-3'>
-                            <img
-                                className='state-to-watch-image'
-                                src='/images/states-to-watch/maine.png'
-                                // eslint-disable-next-line max-len
-                                srcSet='/images/states-to-watch/maine-125w.png 125w, /images/states-to-watch/maine-250w.png 250w'
-                                alt='Outline of the state of Maine'
-                                loading='lazy'
-                            />
-                        </div>
-                        <div>
-                            <strong className='state-to-watch-headline mb-3'>
-                                Maine leads the way in protecting survivors against coerced and fraudulent debt
-                            </strong>
-                            <p className='state-to-watch-text mb-1'>
-                                Maine recently passed strong legislation that prevents debt collectors from collecting
-                                on debts incurred as a result of economic abuse, providing survivors with much needed
-                                relief.
-                            </p>
-                            <a
-                                className='state-to-watch-text'
-                                href='https://www.mainelegislature.org/legis/statutes/10/title10sec1310-H.html'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                            >
-                                Me. Stat. tit. 10, § 1310-H
-                            </a>
-                        </div>
-                    </div>
-                    <div 
-                        className='d-flex flex-row flex-wrap flex-md-nowrap mb-4'
-                        style={{ backgroundColor: '#fff', border: '1px solid #DDDDDD', padding: '1.5rem'}}
-                    >
-                        <div className='mb-2 mb-md-0 mr-md-3'>
-                            <img
-                                className='state-to-watch-image'
-                                src='/images/states-to-watch/washington.png'
-                                // eslint-disable-next-line max-len
-                                srcSet='/images/states-to-watch/washington-125w.png 125w, /images/states-to-watch/washington-250w.png 250w'
-                                alt='outline of the state of Washington'
-                                loading='lazy'
-                            />
-                        </div>
-                        <div>
-                            <strong className='state-to-watch-headline mb-3'>
-                                Washington demonstrates how to protect survivors against litigation abuse
-                            </strong>
-                            <p className='state-to-watch-text mb-1'>
-                                Washington gives survivors the most comprehensive protections against litigation abuse
-                                in the nation. The State requires harm-doers to pay attorneys’ fees and costs associated
-                                with abusive litigation tactics, holding them accountable for misusing the court system
-                                to further harm and abuse survivors.
-                            </p>
-                            <a
-                                className='state-to-watch-text'
-                                href='https://app.leg.wa.gov/RCW/default.aspx?cite=26.51&full=true#26.51.010'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                            >
-                                Wash. Rev. Code § 26.51.010 - 901
-                            </a>
-                        </div>
-                    </div>
-                    <div 
-                        className='d-flex flex-row flex-wrap flex-md-nowrap mb-4'
-                        style={{ backgroundColor: '#fff', border: '1px solid #DDDDDD', padding: '1.5rem'}}
-                    >
-                        <div className='mb-2 mb-md-0 mr-md-3'>
-                            <img
-                                className='state-to-watch-image'
-                                src='/images/states-to-watch/nevada.png'
-                                // eslint-disable-next-line max-len
-                                srcSet='/images/states-to-watch/nevada-125w.png 125w, /images/states-to-watch/nevada-250w.png 250w'
-                                alt='outline of the state of Nevada'
-                                loading='lazy'
-                            />
-                        </div>
-                        <div className='pr-3'>
-                            <strong className='state-to-watch-headline mb-3'>
-                                Nevada is <em>so close</em> to model paid and protected leave
-                            </strong>
-                            <p className='state-to-watch-text mb-1'>
-                                Nevada offers at least 10 days of protected leave for survivors to deal with the
-                                consequences of abuse that does not deplete accrued leave. If Nevada were to guarantee
-                                that the leave is paid it would be a Model policy.
-                            </p>
-                            <a
-                                className='state-to-watch-text'
-                                href='https://www.leg.state.nv.us/nrs/nrs-608.html#NRS608Sec0198'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                            >
-                                N.R.S. § 608.0198
-                            </a>
-                        </div>
-                    </div>
-                </div>
+      <h2 className='mt-5'>Snapshot of Survivor Financial Security Policies By State</h2>
+      <Snapshots />
 
-                <h2 className='mt-5'>Snapshot of Survivor Financial Security Policies By State</h2>
-                <Snapshots />
-
-                <h2 className='mt-5'>Snapshot of Survivor Financial Security Policies By Category</h2>
-                <div className='d-flex flex-column col-12 col-lg-10 p-0'>
-                    <img
-                        className='img-fluid'
-                        src='/images/snapshot-by-category.png'
-                        alt='snapshot of survivor financial security policies by category'
-                        loading='lazy'
-                    />
-                </div>
-
-                <TakeAction />
-            </>
-        </SharedLayout>
-    )
+      <h2 className='mt-5'>Snapshot of Survivor Financial Security Policies By Category</h2>
+      <div className='d-flex flex-column col-12 col-lg-10 p-0'>
+        <img
+          className='img-fluid'
+          src='/images/snapshot-by-category.png'
+          alt='snapshot of survivor financial security policies by category'
+          loading='lazy'
+        />
+      </div>
+      <TakeAction />
+    </SharedLayout>
+  )
 }
 
 Home.propTypes = {
-    states: PropTypes.arrayOf(
-        PropTypes.shape({
-            grade: PropTypes.shape({
-                grade: PropTypes.number
-            }),
-            name: PropTypes.string
-        })
-    )
+  allStates: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      grade: PropTypes.shape({
+        grade: PropTypes.number
+      })
+    })
+  ),
+  watchlistStates: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      watchlistPosition: PropTypes.number,
+      watchlistContent: PropTypes.object
+    })
+  )
 }
 
 export async function getStaticProps() {
-    let states = await getStates()
-    // Assign rank to states list
-    let lastTotal = Infinity
-    let lastRank = 0
-    states = states
-        .sort((a, b) => b.total - a.total)
-        .map((state, i) => {
-            if (state.total === lastTotal) {
-                // If there's a tie, use the previous rank.
-                state.rank = lastRank
-            }
-            if (state.total < lastTotal) {
-                // Get the next ranking (accounting for the gap left by any
-                // previous ties).
-                const newRank = i + 1 > lastRank ? i + 1 : lastRank + 1
-                // Update the last rank/total and set the state rank value
-                state.rank = lastRank = newRank
-                lastTotal = state.total
-            }
-            return state
-        })
-    return {
-        props: {
-            states
-        },
-        revalidate: 5 * 60 // regenerate the page if it's been more than 5 minutes
-    }
+  let allStates = await getStates()
+  // Assign rank to states list
+  let lastTotal = Infinity
+  let lastRank = 0
+
+  allStates = allStates
+    .sort((a, b) => b.total - a.total)
+    .map((state, i) => {
+      if (state.total === lastTotal) {
+        // If there's a tie, use the previous rank.
+        state.rank = lastRank
+      }
+      if (state.total < lastTotal) {
+        // Get the next ranking (accounting for the gap left by any previous ties).
+        const newRank = i + 1 > lastRank ? i + 1 : lastRank + 1
+        // Update the last rank/total and set the state rank value
+        state.rank = lastRank = newRank
+        lastTotal = state.total
+      }
+      return state
+    })
+
+  let watchlistStates = allStates
+    .filter((state) => state.watchlistPosition !== null)
+    .sort((stateA, stateB) => stateA.watchlistPosition - stateB.watchlistPosition)
+
+  return {
+    props: {
+      allStates,
+      watchlistStates
+    },
+    revalidate: 5 * 60 // regenerate the page if it's been more than 5 minutes
+  }
 }
 
 export default Home
